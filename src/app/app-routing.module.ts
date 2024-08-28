d9ef8919ba34a837c5d555687688c48bbfaa2eb8
nermine <baatoutnermine@gmail.com>
2024-08-28 11:58:21 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OfferDetailComponent } from './offer-detail/offer-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "/detail", component: OfferDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { OfferDetailComponent } from './offer-detail/offer-detail.component';
+import { CategoryComponent } from './category/category.component';
+import { OffersComponent } from './offers/offers.component';
+import { BannerComponent } from './banner/banner.component';
+import { AboutSiteComponent } from './about-site/about-site.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { BlogAreaComponent } from './blog-area/blog-area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    ContactComponent,
+    FooterComponent,
+    OfferDetailComponent,
+    CategoryComponent,
+    OffersComponent,
+    BannerComponent,
+    AboutSiteComponent,
+    TestimonialComponent,
+    BlogAreaComponent
   ],
   imports: [
     BrowserModule,
